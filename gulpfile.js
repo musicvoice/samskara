3129ae96499bb5576b35a3f45f21f9951ffea8eb
Vasanth Developer <vasanthdevelopers@gmail.com>
2018-12-02 23:10:44 +0000

    GulpJS now compiles SASS code
    1. Added gulp as dependency
    2. Added node-sass, gulp-sass, gulp-sourcemaps, gulp-autoprefixer and gulp-csso as dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// Importing the required modules
+const gulp = require('gulp'),
+	  sass = require('gulp-sass'),
+	  autoprefixer = require('gulp-autoprefixer'),
+	  sourcemaps = require('gulp-sourcemaps'),
+	  csso = require('gulp-csso');
+
+// Setting the sass compiler as node-sass
+sass.compiler = require('node-sass');
+
+// Setting SASS compiler settings
+var SassOptions = {
+	outputStyle: 'expanded'
+};
+
+// The Gulp task for preparing CSS
+gulp.task('sass', function() {
+	return gulp.src('./sources/sass/**/*.+(scss|sass)')
+		.pipe(sourcemaps.init())
+		.pipe(sass(SassOptions).on('error', sass.logError))
+		.pipe(autoprefixer({browsers: ['last 4 versions']}))
+		.pipe(csso())
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(gulp.dest('./assets/css'));
+});
